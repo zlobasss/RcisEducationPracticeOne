d95bc624000964df4e286f4365adfe8dcbd5c39d
Злобин Сергей Сергеевич <gr612_zlsse@ttit.local>
2022-10-18 10:48:10 +0700

    First commit. Create console apps

---
+++ rcis_ep_task_one/bin/rcis_ep_task_one.dart
@@ -0,0 +1,17 @@
+import 'package:rcis_ep_task_one/rcis_ep_task_one.dart' as rcis_ep_task_one;
+import 'dart:math';
+
+void main(List<String> arguments) {
+  List<int> mass = List.generate(10, (_) => Random().nextInt(10));
+  
+  int min = 0;
+
+  for(int i = 1; i < 10; ++i){
+    if(mass[i] < mass[min]){
+      min = i;
+    }
+  }
+
+  print(mass);
+  print(min);
+}
